f687390de11a721836e78bb3953307269902c056
Sebastian <eljhoan1411@gmail.com>
2023-04-02 23:49:51 -0500

    add cors origin

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.beca import beca
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(beca)
 @app.get('/')
