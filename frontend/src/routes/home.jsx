a0d49e13e82fc28b53f7c6bbb837b8be577a01a5
Sebastian <eljhoan1411@gmail.com>
2023-04-20 01:29:21 -0500

    refactor home route and main file

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,17 +1,12 @@
 import NavBar from '../components/navbar';
 import Card from '../components/card';
-import { obtenerBeca } from '../data/becasApi';
-
-const becas = obtenerBeca(1)
+import Becas from '../components/becas';
 
 const Home = ()=>{
     return (
         <div>
         <NavBar />
-        <Card />
-        <div>
-            <h1>{becas}</h1>
-        </div>
+        <Becas />
         </div>
           
     )
