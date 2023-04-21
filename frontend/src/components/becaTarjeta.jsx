78202b3cca1a7597e1414d7263c9fb86a518be46
Sebastian <eljhoan1411@gmail.com>
2023-04-21 01:22:27 -0500

    add dynamic route to detail

--- frontend/src/components/becaTarjeta.jsx
+++ frontend/src/components/becaTarjeta.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const BecaTarjeta = (props) => {
     return (
         <div className="card w-96 bg-neutral text-neutral-content my-6 mx-auto"  >
         <div className="card-body items-center text-center">
           <h2 className="card-title mb-4" >{props.beca.name}</h2>
-          <p>Niveles de estudio: {props.beca.study_level}</p>
+          {/* <p>Niveles de estudio: {props.beca.study_level}</p> */}
           <div className="card-actions justify-end">
-            <button class="btn btn-accent btn-outline"><a href="/detail">Accept</a></button>
+            <button class="btn btn-accent btn-outline"><Link to={`/detail/${props.beca.id}`}>Detalles y recomendaciones</Link></button>
           </div>
         </div>
       </div>
