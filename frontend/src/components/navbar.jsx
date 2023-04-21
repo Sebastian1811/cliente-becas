1ae29ae8642265153a4a154e0e7932749f0c38ff
Sebastian <eljhoan1411@gmail.com>
2023-04-21 01:22:47 -0500

    add home and about buttons

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "../styles/tailwind.css";
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
     return(
+    // <div className="navbar bg-base-100">
+    // <a className="btn btn-ghost normal-case text-xl">MatchWise</a>
+    // </div>
     <div className="navbar bg-base-100">
-    <a className="btn btn-ghost normal-case text-xl">MatchWise</a>
+        <div className="flex-1">
+            <Link to={'/'} className="btn btn-ghost normal-case text-xl">MatchWise</Link>
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+            <li><Link to={'/'}>Home</Link></li>
+            <li><a>About</a></li>
+            </ul>
+        </div>
     </div>
     )
 }
