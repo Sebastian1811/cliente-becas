a39215cedb1439271518a27463368a2dbe0f64cd
Sebastian <eljhoan1411@gmail.com>
2023-04-21 01:22:04 -0500

    add dinamyc routing for beca detail

--- frontend/src/components/becaDetail.jsx
+++ frontend/src/components/becaDetail.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import "../styles/tailwind.css";
-
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CountryHostList from './countryHostList'
+import Recomendaciones from './recomendaciones'
+import NavBar from "./navbar";
 const BecaDetails = () => {
-  return (
+
+  const { id } = useParams();
+  const [beca, setBeca] = useState([]);
+    useEffect(() => {
+      axios.get(`http://localhost:3000/beca/${id}`)
+        .then(response => setBeca(response.data))
+        .catch(error => console.log(error));
+    }, [id]);
+
+  return(
+    <div>
+    <NavBar/>
     <div className="flex flex-col gap-3 m-10">
-      <div className="bg-blue-600 text-white p-5">
-        <h1 className="m-0">Título</h1>
-      </div>
-      <div className="bg-gray-100 p-5">
-        <h2 className="m-0">Descripción</h2>
-        <p className="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-      <div className="bg-gray-300 p-5">
-        <h2 className="m-0">Requisitos</h2>
-        <ul className="m-0">
-          <li>Requisito 1</li>
-          <li>Requisito 2</li>
-          <li>Requisito 3</li>
-        </ul>
-      </div>
-      <div className="bg-gray-100 p-5">
-        <h2 className="m-0">Países</h2>
-        <ol className="m-0">
-          <li>País 1</li>
-          <li>País 2</li>
-          <li>País 3</li>
-        </ol>
-      </div>
+        <div className="bg-blue-600 text-white p-5">
+          <h1 className="m-0">{beca.name}</h1>
+        </div>
+          <CountryHostList id={id}/>
+          <div className="bg-gray-300 p-5">
+            <h2 className="m-0">Niveles de estudio</h2>
+              <p>{beca.study_level}</p>
+          </div>
+          <div className="bg-gray-300 p-5">
+            <h2 className="m-0">Requisitos</h2>
+              <p>{beca.requirements}</p>
+          </div>
+          <div >
+            <h1 className="m-0">Listado de Recomendaciones</h1>
+            <Recomendaciones id={id} />
+          </div>
     </div>
-  );
+    </div>
+  )
+   
 };
 
 
