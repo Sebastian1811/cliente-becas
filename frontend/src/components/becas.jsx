7f7953ca697c92b99cf5871003f000f7d4ae9efa
Sebastian <eljhoan1411@gmail.com>
2023-06-17 00:54:23 -0500

    improvements in pagination

--- frontend/src/components/becas.jsx
+++ frontend/src/components/becas.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ListaBecas from "./becaLista";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 
 const Becas = () => {
 
@@ -14,23 +14,43 @@ const Becas = () => {
       .then(response => setBecas(response.data))
       .catch(error => console.log(error));
     }, []);
-    console.log(becas)
+    
+    const [items, setItems] = useState(becas)
+
+    // useEffect(() => {
+    //   setItems(becas.splice(0,ITEMS_PER_PAGE));
+    // }, [becas]);
+
+    // console.log('soy copy',copy)
+    console.log('soy items',items)
+    const [currentPage, setCurrentPage] =  useState(0)
+
     const nextHandler = () => {
+      console.log('estoy')
       const TotalElements = becas.length
-      console.log(TotalElements)
+      console.log('estoy2')
+      const nextPage = currentPage + 1
+      console.log('estoy3')
+      const firstIndex = nextPage * ITEMS_PER_PAGE
+      console.log('estoy4', firstIndex, TotalElements)
+      if (firstIndex >= TotalElements) return
+      console.log(becas)
+      setItems(becas.splice(firstIndex,ITEMS_PER_PAGE))
+      setCurrentPage(nextPage)
+      // console.log(TotalElements)
+      console.log('hola', nextPage)
     }
 
     const prevHandler = () => {
         console.log("anterior pagina")
     }
-    const [currentPage, setCurrentPage] =  useState(0)
     return (
       <div>
         <h1 className=" p-3 font-title text-purple-500 inline-flex text-lg transition-all duration-200 md:text-3xl ml-4 mt-6">Listado de becas</h1>
         <br/>
         <button onClick={prevHandler} className="btn btn-accent btn-outline mr-10 mb-8  ml-10" >Prev</button>
         <button onClick={nextHandler} className="btn btn-accent btn-outline mr-10 mb-8  ml-10" >Next</button>
-        <ListaBecas page={currentPage} becas={becas.splice(0,ITEMS_PER_PAGE)} />
+        <ListaBecas page={currentPage} becas={becas} />
       </div>
     );
   }
