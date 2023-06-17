15049d6577e29449d46ffd9985d78cb67c240d39
Sebastian <eljhoan1411@gmail.com>
2023-06-17 15:07:01 -0500

    fix redirect button

--- frontend/src/components/becas.jsx
+++ frontend/src/components/becas.jsx
@@ -48,7 +48,7 @@ const Becas = (props) => {
     if (currentPage < 0){
       return(
        <div className="flex justify-center items-center mt-20 mb-20">
-        <Link to={`/`}className=" font-bold  hover:bg-green-700 btn alert alert-warning w-1/4 flex justify-center items-center ">
+        <Link to={`/`}className=" font-bold  hover:bg-green-700 btn alert alert-warning w-1/3 flex justify-center items-center ">
         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
         Parece que te perdiste si quieres volver haz click
       </Link>
